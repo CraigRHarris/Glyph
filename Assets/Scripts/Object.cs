6fb62261da0c47985b5d631335a4850ef27db58f
DanielSparkes <156078984+DanielSparkes@users.noreply.github.com>
2024-10-17 13:39:00 +0100

    Commit

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -17,9 +17,9 @@ public class Object : MonoBehaviour
 
     void Start()
     {
-        //ScoreScript = Score.GetComponent<Score>();
-        //InputScript = Canvas.GetComponent<MouseInput>();
-        //MeterScript = Slider.GetComponent<SurvivalMeter>();
+        ScoreScript = Score.GetComponent<Score>();
+        InputScript = Canvas.GetComponent<MouseInput>();
+        MeterScript = Slider.GetComponent<SurvivalMeter>();
     }
 
     void Update()
@@ -27,22 +27,22 @@ public class Object : MonoBehaviour
 
         z += 20 * Time.deltaTime;
         transform.position = new Vector3(0, 1, z);
-        //if (InputScript.input == direction)
-        //{
-        //    input = "null";
-        //    ScoreScript.trigger = 1;
-        //    MeterScript.trigger = 1;
-        //    Destroy(gameObject);
-        //}
-        //if (InputScript.input != direction & InputScript.input != input &(InputScript.input == "Right" | InputScript.input == "Left" | InputScript.input == "Up" | InputScript.input == "Down"))
-        //{
-        //    input = InputScript.input;
-        //    MeterScript.trigger = 2;
-        //}
-        //if (z > 40f)
-        //{
-        //    input = "null";
-        //    Destroy(gameObject);
-        //}
+        if (InputScript.input == direction)
+        {
+            input = "null";
+            ScoreScript.trigger = 1;
+            MeterScript.trigger = 1;
+            Destroy(gameObject);
+        }
+        if (InputScript.input != direction & InputScript.input != input &(InputScript.input == "Right" | InputScript.input == "Left" | InputScript.input == "Up" | InputScript.input == "Down"))
+        {
+            input = InputScript.input;
+            MeterScript.trigger = 2;
+        }
+        if (z > 40f)
+        {
+            input = "null";
+            Destroy(gameObject);
+        }
     }
 }
